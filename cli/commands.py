4e63a1626681c4632c3c4e6628452a0b1db5a38e
yoseoki <erhnknum18@gmail.com>
2025-07-07 15:53:42 +0900

    re-change style of giving args

--- cli/commands.py
+++ cli/commands.py
@@ -6,20 +6,19 @@ import services.db as DB
 import services.faissdb as FDB
 import services.notion as NOTION
 
-def add_specific_paper_to_db():
+def add_specific_paper_to_db(arxiv_ids):
     crawler = CRAWLER.ArxivAPIClient()
     dbms = DB.MySQLClient()
 
-    arxiv_id = input("Please input arxiv_id for update db(ex. 2407.04307v1) : ")
-    papers = crawler.search_arxiv_with_id(arxiv_id)
+    if str(type(arxiv_ids)) == "<class 'str'>": papers = crawler.search_arxiv_with_ids([arxiv_ids])
+    else: papers = crawler.search_arxiv_with_ids(arxiv_ids)
     dbms.save_papers(papers)
     print("create over!")
 
-def add_query_paper_to_db():
+def add_query_paper_to_db(query):
     crawler = CRAWLER.ArxivAPIClient()
     dbms = DB.MySQLClient()
 
-    query = input("Please input keywords for update db(ex. grassmann manifold learning) : ")
     papers = crawler.search_arxiv_with_query(query)
     dbms.save_papers(papers)
     print("create over!")
@@ -32,23 +31,22 @@ def update():
     faiss_dbms.add(papers)
     print("update over!")
 
-def translate_specific_paper():
+def translate_specific_paper(arxiv_ids):
     dbms = DB.MySQLClient()
     genai = AI.GenAIClient()
     notion = NOTION.NotionClient()
 
-    arxiv_id = input("Please input arxiv_id for translate(ex. 2407.04307v1) : ")
-    papers = dbms.fetch_papers_by_arxiv_ids([arxiv_id])
+    if str(type(arxiv_ids)) == "<class 'str'>": papers = dbms.fetch_papers_by_arxiv_ids([arxiv_ids])
+    else: papers = dbms.fetch_papers_by_arxiv_ids(arxiv_ids)
     translated_papers = genai.translate_abstract(papers)
     notion.upload(papers, translated_papers)
     print("create over!")
 
-def translate_query_paper():
+def translate_query_paper(query):
     genai = AI.GenAIClient()
     faiss_dbms = FDB.FAISSClient()
     notion = NOTION.NotionClient()
 
-    query = input("Please input keywords for query FAISS db(ex. grassmann manifold learning) : ")
     papers = faiss_dbms.query(query)
     translated_papers = genai.translate_abstract(papers)
     notion.upload(papers, translated_papers)
