be06f740a199dfa9b07bb0c5dfee4e5568d2cd8c
yoseoki <erhnknum18@gmail.com>
2025-07-07 15:29:26 +0900

    add new function 'translate_specific_paper

--- cli/commands.py
+++ cli/commands.py
@@ -4,6 +4,7 @@ import services.ai as AI
 import services.crawler as CRAWLER
 import services.db as DB
 import services.faissdb as FDB
+import services.notion as NOTION
 
 def add_specific_paper_to_db():
     crawler = CRAWLER.ArxivAPIClient()
@@ -12,6 +13,7 @@ def add_specific_paper_to_db():
     arxiv_id = input("Please input arxiv_id for update db(ex. 2407.04307v1) : ")
     papers = crawler.search_arxiv_with_id(arxiv_id)
     dbms.save_papers(papers)
+    print("create over!")
 
 def add_query_paper_to_db():
     crawler = CRAWLER.ArxivAPIClient()
@@ -20,6 +22,7 @@ def add_query_paper_to_db():
     query = input("Please input keywords for update db(ex. grassmann manifold learning) : ")
     papers = crawler.search_arxiv_with_query(query)
     dbms.save_papers(papers)
+    print("create over!")
 
 def update():
     dbms = DB.MySQLClient()
@@ -28,4 +31,15 @@ def update():
     papers = dbms.fetch_papers_by_unembed()
     faiss_dbms.add(papers)
     print("update over!")
+
+def translate_specific_paper():
+    dbms = DB.MySQLClient()
+    genai = AI.GenAIClient()
+    notion = NOTION.NotionClient()
+
+    arxiv_id = input("Please input arxiv_id for translate(ex. 2407.04307v1) : ")
+    papers = dbms.fetch_papers_by_arxiv_ids([arxiv_id])
+    translated_papers = genai.translate_abstract(papers)
+    notion.upload(papers, translated_papers)
+    print("create over!")
 
