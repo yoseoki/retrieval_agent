2759f55f6220c2fb7cb51ecb9b378c5642fd7f68
yoseoki <erhnknum18@gmail.com>
2025-07-07 15:57:21 +0900

    reform main.py to match input form of commands

--- cli/main.py
+++ cli/main.py
@@ -2,5 +2,12 @@
 
 from .commands import *
 
-arxiv_id = input("Please input arxiv id (ex. 2201.05229v1) : ")
-add_specific_paper_to_db(arxiv_id)
+# arxiv_ids = input("Please input arxiv id (ex. 2201.05229v1) : ")
+# add_specific_paper_to_db(arxiv_ids)
+# translate_specific_paper(arxiv_ids)
+
+# update()
+
+# query = input("Please input keywords for update db(ex. grassmann manifold learning) : ")
+# add_query_paper_to_db(query)
+# translate_query_paper(query)
