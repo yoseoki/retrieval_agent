f131f9e9967203cff1bf48e7f52e152dd0cddb19
yoseoki <erhnknum18@gmail.com>
2025-07-07 15:20:01 +0900

    change how to give args of add_specific_paper_to_db

--- cli/commands.py
+++ cli/commands.py
@@ -5,9 +5,10 @@ import services.crawler as CRAWLER
 import services.db as DB
 import services.faissdb as FDB
 
-def add_specific_paper_to_db(arxiv_id):
+def add_specific_paper_to_db():
     crawler = CRAWLER.ArxivAPIClient()
     dbms = DB.MySQLClient()
 
+    arxiv_id = input("Please input arxiv_id for update db(ex. 2407.04307v1) : ")
     papers = crawler.search_arxiv_with_id(arxiv_id)
     dbms.save_papers(papers)
