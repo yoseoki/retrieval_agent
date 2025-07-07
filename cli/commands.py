b53277b4e9a3f6eacc551e42afd36e4f887b1146
yoseoki <erhnknum18@gmail.com>
2025-07-07 15:22:09 +0900

    add new function 'add_query_paper_to_db'

--- cli/commands.py
+++ cli/commands.py
@@ -12,3 +12,11 @@ def add_specific_paper_to_db():
     arxiv_id = input("Please input arxiv_id for update db(ex. 2407.04307v1) : ")
     papers = crawler.search_arxiv_with_id(arxiv_id)
     dbms.save_papers(papers)
+
+def add_query_paper_to_db():
+    crawler = CRAWLER.ArxivAPIClient()
+    dbms = DB.MySQLClient()
+
+    query = input("Please input keywords for update db(ex. grassmann manifold learning) : ")
+    papers = crawler.search_arxiv_with_query(query)
+    dbms.save_papers(papers)
