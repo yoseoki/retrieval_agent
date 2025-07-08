567b3a90fa6fea98eb85ba8c54a887af39fdc73b
yoseoki <erhnknum18@gmail.com>
2025-07-08 11:15:17 +0900

    add logging system to all methods

--- services/crawler.py
+++ services/crawler.py
@@ -30,6 +30,7 @@ class ArxivAPIClient():
         return papers
     
     def search_arxiv_with_query(self, query, max_results=30):
+        print("CRAWL : Trying to crawl papers by query...", end=" ")
         params = {
             "search_query": query,
             "start": 0,
@@ -40,9 +41,11 @@ class ArxivAPIClient():
         response = requests.get(self.base_url, params=params)
         response.raise_for_status()
         papers = self._parse_entries(response.text)
+        print(f"OK, {len(papers)} papers found.")
         return papers
     
     def search_arxiv_with_ids(self, arxiv_ids):
+        print(f"CRAWL : Trying to crawl {len(arxiv_ids)} papers...", end=" ")
         id_list_str = ",".join(arxiv_ids)
         params = {
             "id_list": id_list_str
@@ -50,4 +53,5 @@ class ArxivAPIClient():
         response = requests.get(self.base_url, params=params)
         response.raise_for_status()
         papers = self._parse_entries(response.text)
+        print(f"OK, {len(papers)} papers found.")
         return papers
